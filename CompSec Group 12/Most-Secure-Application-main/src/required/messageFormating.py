0e9fe46dca746b3df50ae7ef0fd1c876adbe72c6
Güneş Özmen <91736429+Goxmen@users.noreply.github.com>
2023-11-17 20:06:48 +0100

    Add files via upload

---
+++ CompSec Group 12/Most-Secure-Application-main/src/required/messageFormating.py
@@ -0,0 +1,33 @@
+FORMAT = 'utf-8'
+HEADER = 64
+
+def encode_message(msg, conn):
+    encodedMsg = msg.encode(FORMAT)
+    msgLength = str(len(encodedMsg)).encode(FORMAT) # Encode length of message.
+    msgLength += b' ' * (HEADER - len(msgLength)) # Pad length message to 64 bytes.
+    conn.send(msgLength)
+    conn.send(encodedMsg)
+
+def decode_message(conn):
+    msgLength = conn.recv(HEADER).decode(FORMAT) # Get incoming message's length.
+    message = ""
+    if msgLength:
+        msgLength = int(msgLength)
+        message = conn.recv(msgLength).decode(FORMAT) # Decode actual message.
+    return message
+    
+def encrypt_send(msg, conn, f_key):
+    encrypted = f_key.encrypt(msg.encode())
+    length = str(len(encrypted)).encode(FORMAT)
+    length += b' ' * (HEADER - len(length))
+    conn.send(length)
+    conn.send(encrypted)
+
+def receive_decrypt(conn, f_key):
+    length = conn.recv(HEADER).decode(FORMAT)
+    encryption = ""
+    if length:
+        length = int(length)
+        encryption = conn.recv(length)
+        return f_key.decrypt(encryption).decode()
+    
